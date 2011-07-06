0da679d279f8ad9bd8445438aa65b3786d6c15a8
Ivo Jimenez <ivo@cs.ucsc.edu>
2011-07-05 23:42:23 -0700

    issue #80 created Advisor interface
    also added small package-level documentation and empty WFIT class

---
+++ src/edu/ucsc/dbtune/advisor/WFIT.java
@@ -0,0 +1,55 @@
+/* ************************************************************************** *
+ *   Copyright 2010 University of California Santa Cruz                       *
+ *                                                                            *
+ *   Licensed under the Apache License, Version 2.0 (the "License");          *
+ *   you may not use this file except in compliance with the License.         *
+ *   You may obtain a copy of the License at                                  *
+ *                                                                            *
+ *       http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                            *
+ *   Unless required by applicable law or agreed to in writing, software      *
+ *   distributed under the License is distributed on an "AS IS" BASIS,        *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied  *
+ *   See the License for the specific language governing permissions and      *
+ *   limitations under the License.                                           *
+ * ************************************************************************** */
+package edu.ucsc.dbtune.advisor;
+
+import edu.ucsc.dbtune.util.IndexBitSet;
+
+import java.sql.SQLException;
+
+/**
+ * Represents an advisor that recommends physical design modifications.
+ */
+public class WFIT extends Advisor
+{
+	/**
+     * Adds a query to the set of queries that are considered for recommendation.
+     *
+     * @param sql
+     *      sql statement
+     * @throws SQLException
+     *      if the given statement can't be processed
+     */
+    @Override
+    public void process(String sql) throws SQLException
+    {
+        throw new SQLException("Not implemented yet");
+    }
+
+    /**
+     * Returns the configuration obtained by the Advisor.
+     *
+     * @return
+     *      a {@code Configuration} object containing the information related to
+     *      the recommendation produced by the advisor.
+     * @throws SQLException
+     *      if the given statement can't be processed
+     */
+    @Override
+    public IndexBitSet getRecommendation() throws SQLException
+    {
+        throw new SQLException("Not implemented yet");
+    }
+}
