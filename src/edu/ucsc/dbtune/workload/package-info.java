dd52287cf2a290818d055c1feea3c9e0e544fb07
Ivo Jimenez <ivo@cs.ucsc.edu>
2011-12-13 01:22:33 -0800

    issue #51 Removing license headers

--- src/edu/ucsc/dbtune/workload/package-info.java
+++ src/edu/ucsc/dbtune/workload/package-info.java
@@ -1,22 +1,7 @@
-/* **************************************************************************** *
- *   Copyright 2010 University of California Santa Cruz                         *
- *                                                                              *
- *   Licensed under the Apache License, Version 2.0 (the "License");            *
- *   you may not use this file except in compliance with the License.           *
- *   You may obtain a copy of the License at                                    *
- *                                                                              *
- *       http://www.apache.org/licenses/LICENSE-2.0                             *
- *                                                                              *
- *   Unless required by applicable law or agreed to in writing, software        *
- *   distributed under the License is distributed on an "AS IS" BASIS,          *
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- *   See the License for the specific language governing permissions and        *
- *   limitations under the License.                                             *
- * **************************************************************************** */
 /**
- * Definition of the {@link edu.ucsc.dbtune.workload.SQLStatement} class. This abstraction allows to easily manipulate SQL 
- * statements, for example, if there's the need of identifying if a statement is being submitted again to an {@link 
- * edu.ucsc.dbtune.optimizer.Optimizer}.
+ * Definition of the {@link edu.ucsc.dbtune.workload.SQLStatement} class. This abstraction allows to 
+ * easily manipulate SQL statements, for example, if there's the need of identifying if a statement 
+ * is being submitted again to an {@link edu.ucsc.dbtune.optimizer.Optimizer}.
  * <p>
  * The {@link edu.ucsc.dbtune.workload.Workload} class facilitates the manipulation of SQL scripts.
  */
