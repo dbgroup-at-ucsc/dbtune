7afef2a2f6258603fa082bea07aaa1b5583b50d4
Huascar A. Sanchez <huascar.sanchez@gmail.com>
2011-12-21 09:36:53 -0800

    fixed a problem with the Key#intersects(Configuration first, Configuration second) method. Now the requirement "every member of first must is a member of second" is satisfied.

--- extensions/inum/src/edu/ucsc/dbtune/inum/Key.java
+++ extensions/inum/src/edu/ucsc/dbtune/inum/Key.java
@@ -18,21 +18,41 @@ import edu.ucsc.dbtune.workload.SQLStatement;
  *
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
-public class Key
-{
-    private final SQLStatement  sql;
-    private final Set<Index> configuration;
+public class Key {
+  private final SQLStatement  sql;
+  private final Configuration configuration;
+  public Key(String sql, Configuration configuration) {
+    this(createStatement(sql), configuration);
+  }
 
-    public Key(String sql, Set<Index> configuration)
-    {
-        this(createStatement(sql), configuration);
+  public Key(SQLStatement sql, Configuration configuration){
+    this.sql           = sql;
+    this.configuration = configuration;
+  }
+
+  @Override public int hashCode() {
+    return Objects.hashCode(sql, configuration);//(sql + configuration.toString()).hashCode();
+  }
+
+  @Override public boolean equals(Object o) {
+    if(!(o instanceof Key)) return false;
+    final Key other = (Key)o;
+    return isCoveredBy(other.sql, other.configuration);
+  }
+
+  private static SQLStatement createStatement(String sql){
+    Preconditions.checkArgument(!Strings.isEmpty(sql), "SQL query is empty.");
+    try {
+      return new SQLStatement(sql);
+    } catch (Exception e){
+      Console.streaming().error("Unable to create SQL Statement", e);
+      throw new RuntimeException(e);
     }
 
-    public Key(SQLStatement sql, Set<Index> configuration)
-    {
-        this.sql           = sql;
-        this.configuration = configuration;
-    }
+  private boolean isCoveredBy(SQLStatement sql, Configuration other){
+    final boolean equalSQL    = Strings.same(this.sql.getSQL(), sql.getSQL());
+    final boolean intersected = intersects(configuration, other);
+    final boolean sameConfig  = configuration.equals(other);
 
     @Override
     public int hashCode()
@@ -57,37 +77,10 @@ public class Key
             throw new RuntimeException(e);
         }
     }
-
-    private boolean isMatch(SQLStatement sql, Set<Index> other)
-    {
-        final boolean equalSQL    = Strings.same(this.sql.getSQL(), sql.getSQL());
-        final boolean intersected = intersects(configuration, other);
-        final boolean sameConfig  = configuration.equals(other);
-
-        return ((equalSQL && intersected) || (equalSQL && sameConfig));
-    }
-
-
-    private static boolean intersects(Set<Index> first, Set<Index> second)
-    {
-        final Set<Index> c = new HashSet<Index>();
-
-        if (first.size() < second.size()) {
-            for (Index x : first) {
-                if (second.contains(x)) {
-                    c.add(x);
-                }
-            }
-        } else {
-            for (Index x : second) {
-                if (first.contains(x)) {
-                    c.add(x);
-                }
-            }
-        }
-
-        return !c.isEmpty();
-    }
+    //  'first' is a subset of 'second' iff every member of 'first' is a member of 'second'
+    final boolean isEveryMemberInSecond = c.size() == first.size();
+    return !c.toList().isEmpty() && isEveryMemberInSecond;
+  }
 
     @Override
     public String toString() {
