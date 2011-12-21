59d8c428c417953b4de88149f3fee9d572117fb3
Quoc Trung Tran <tqtrung@soe.ucsc.edu>
2011-12-21 11:33:09 -0800

    issue #129 Create a test for Interaction
    Create an abstract class that instantiates database schema and workload uses for all the BIP test.
    Finish the testing on interaction.

---
+++ extensions/bip/tests/edu/ucsc/dbtune/bip/QueryPlanDescTest.java
@@ -0,0 +1,67 @@
+package edu.ucsc.dbtune.bip;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import edu.ucsc.dbtune.bip.util.QueryPlanDesc;
+
+public class QueryPlanDescTest extends BIPTestConfiguration 
+{
+    @Test
+    public void testPlanDescriptionGeneration() throws Exception
+    {           
+        for (int q = 0; q < numQ; q++) {
+            QueryPlanDesc desc = new  QueryPlanDesc();
+            desc.generateQueryPlanDesc(listInum.get(q), candidateIndexes);
+    
+            assertThat(desc.getNumSlots(), is(numSchemaTables));
+            assertThat(desc.getNumPlans(), is(numPlans[q]));
+            
+            for (int k = 0; k < desc.getNumPlans(); k++) {
+                if (q == 0){
+                    assertThat(desc.getInternalPlanCost(k), is(internalCostPlan1[k]));
+                } else if (q == 1) {
+                    assertThat(desc.getInternalPlanCost(k), is(internalCostPlan2[k]));
+                }
+            }
+            
+            for (int i = 0; i < numSchemaTables; i++) {
+                assertThat(desc.getNumIndexesEachSlot(i), is(3));
+            }
+            
+            int p = 0;
+            for (int k = 0; k < desc.getNumPlans(); k++) {
+                for (int i = 0; i < numSchemaTables; i++) {
+                    
+                    for (int a = 0; a < desc.getNumIndexesEachSlot(i); a++){
+                        
+                        if ( (q == 0 && i <= 1) // the first query
+                             || (q == 1 && i >= 1) // the second query 
+                           ){
+                        
+                            if (a == desc.getNumIndexesEachSlot(i) - 1) {
+                                assertThat(desc.getIndexAccessCost(k, i, a), is(100.0));
+                            }
+                            else {
+                                if (q == 0) {
+                                    assertThat(desc.getIndexAccessCost(k, i, a), 
+                                            is(accessCostPlan1[p]));
+                                }
+                                else if (q == 1) {
+                                    assertThat(desc.getIndexAccessCost(k, i, a), 
+                                            is(accessCostPlan2[p]));
+                                }
+                                p++;
+                            }
+                        } else {
+                            // assert value 0
+                            assertThat(desc.getIndexAccessCost(k, i, a), is(0.0));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("The generator of query plan descriptor is tested SUCCESSFULLY.");
+    }
+}
