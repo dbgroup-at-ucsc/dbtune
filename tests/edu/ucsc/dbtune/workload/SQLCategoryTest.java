27eb855b0626ce4d48837e32b7ec8270fb289775
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-01-04 02:43:51 -0700

    issue #136 Adding more tests for SQLCategory

--- tests/edu/ucsc/dbtune/workload/SQLCategoryTest.java
+++ tests/edu/ucsc/dbtune/workload/SQLCategoryTest.java
@@ -71,41 +71,104 @@ public class SQLCategoryTest
         assertThat(SELECT.contains(INSERT), is(false));
         assertThat(SELECT.contains(DELETE), is(false));
         assertThat(SELECT.contains(UNKNOWN), is(false));
-
-        assertThat(DELETE.contains(SELECT), is(false));
-        assertThat(DELETE.contains(UPDATE), is(false));
-        assertThat(DELETE.contains(INSERT), is(false));
-        assertThat(DELETE.contains(DELETE), is(true));
-        assertThat(DELETE.contains(UNKNOWN), is(false));
+        assertThat(SELECT.contains(NOT_SELECT), is(false));
 
         assertThat(UPDATE.contains(SELECT), is(false));
         assertThat(UPDATE.contains(UPDATE), is(true));
         assertThat(UPDATE.contains(INSERT), is(false));
         assertThat(UPDATE.contains(DELETE), is(false));
         assertThat(UPDATE.contains(UNKNOWN), is(false));
+        assertThat(UPDATE.contains(NOT_SELECT), is(true));
 
         assertThat(INSERT.contains(SELECT), is(false));
         assertThat(INSERT.contains(UPDATE), is(false));
         assertThat(INSERT.contains(INSERT), is(true));
         assertThat(INSERT.contains(DELETE), is(false));
         assertThat(INSERT.contains(UNKNOWN), is(false));
+        assertThat(INSERT.contains(NOT_SELECT), is(true));
 
-        assertThat(INSERT.contains(SELECT), is(false));
-        assertThat(INSERT.contains(UPDATE), is(false));
-        assertThat(INSERT.contains(INSERT), is(true));
-        assertThat(INSERT.contains(DELETE), is(false));
-        assertThat(INSERT.contains(UNKNOWN), is(false));
-
-        assertThat(NOT_SELECT.contains(SELECT), is(false));
-        assertThat(NOT_SELECT.contains(UPDATE), is(true));
-        assertThat(NOT_SELECT.contains("I"), is(true));
-        assertThat(NOT_SELECT.contains(DELETE), is(true));
-        assertThat(NOT_SELECT.contains(UNKNOWN), is(true));
+        assertThat(DELETE.contains(SELECT), is(false));
+        assertThat(DELETE.contains(UPDATE), is(false));
+        assertThat(DELETE.contains(INSERT), is(false));
+        assertThat(DELETE.contains(DELETE), is(true));
+        assertThat(DELETE.contains(UNKNOWN), is(false));
+        assertThat(DELETE.contains(NOT_SELECT), is(true));
 
         assertThat(UNKNOWN.contains(SELECT), is(false));
         assertThat(UNKNOWN.contains(UPDATE), is(false));
-        assertThat(UNKNOWN.contains("I"), is(false));
+        assertThat(UNKNOWN.contains(INSERT), is(false));
         assertThat(UNKNOWN.contains(DELETE), is(false));
         assertThat(UNKNOWN.contains(UNKNOWN), is(true));
+        assertThat(UNKNOWN.contains(NOT_SELECT), is(true));
+
+        assertThat(NOT_SELECT.contains(SELECT), is(false));
+        assertThat(NOT_SELECT.contains(UPDATE), is(true));
+        assertThat(NOT_SELECT.contains(INSERT), is(true));
+        assertThat(NOT_SELECT.contains(DELETE), is(true));
+        assertThat(NOT_SELECT.contains(UNKNOWN), is(true));
+        assertThat(NOT_SELECT.contains(NOT_SELECT), is(true));
+    }
+
+    /**
+     */
+    @Test
+    public void testParsing()
+    {
+        assertThat(SQLCategory.from("S"), is(SQLCategory.SELECT));
+        assertThat(SQLCategory.from("U"), is(SQLCategory.UPDATE));
+        assertThat(SQLCategory.from("I"), is(SQLCategory.INSERT));
+        assertThat(SQLCategory.from("D"), is(SQLCategory.DELETE));
+        assertThat(SQLCategory.from("?"), is(SQLCategory.UNKNOWN));
+        assertThat(SQLCategory.from("SELECT * from blah"), is(SQLCategory.SELECT));
+        assertThat(SQLCategory.from("UPDATE blah SET foo = 0"), is(SQLCategory.UPDATE));
+        assertThat(SQLCategory.from("INSERT INTO blah(foo) VALUES 0"), is(SQLCategory.INSERT));
+    }
+
+    /**
+     */
+    @Test
+    public void testEquality()
+    {
+        assertThat(SELECT.isSame(SELECT), is(true));
+        assertThat(SELECT.isSame(UPDATE), is(false));
+        assertThat(SELECT.isSame(INSERT), is(false));
+        assertThat(SELECT.isSame(DELETE), is(false));
+        assertThat(SELECT.isSame(UNKNOWN), is(false));
+        assertThat(SELECT.isSame(NOT_SELECT), is(false));
+
+        assertThat(DELETE.isSame(SELECT), is(false));
+        assertThat(DELETE.isSame(UPDATE), is(false));
+        assertThat(DELETE.isSame(INSERT), is(false));
+        assertThat(DELETE.isSame(DELETE), is(true));
+        assertThat(DELETE.isSame(UNKNOWN), is(false));
+        assertThat(DELETE.isSame(NOT_SELECT), is(true));
+
+        assertThat(UPDATE.isSame(SELECT), is(false));
+        assertThat(UPDATE.isSame(UPDATE), is(true));
+        assertThat(UPDATE.isSame(INSERT), is(false));
+        assertThat(UPDATE.isSame(DELETE), is(false));
+        assertThat(UPDATE.isSame(UNKNOWN), is(false));
+        assertThat(UPDATE.isSame(NOT_SELECT), is(true));
+
+        assertThat(INSERT.isSame(SELECT), is(false));
+        assertThat(INSERT.isSame(UPDATE), is(false));
+        assertThat(INSERT.isSame(INSERT), is(true));
+        assertThat(INSERT.isSame(DELETE), is(false));
+        assertThat(INSERT.isSame(UNKNOWN), is(false));
+        assertThat(INSERT.isSame(NOT_SELECT), is(true));
+
+        assertThat(NOT_SELECT.isSame(SELECT), is(false));
+        assertThat(NOT_SELECT.isSame(UPDATE), is(true));
+        assertThat(NOT_SELECT.isSame(INSERT), is(true));
+        assertThat(NOT_SELECT.isSame(DELETE), is(true));
+        assertThat(NOT_SELECT.isSame(UNKNOWN), is(true));
+        assertThat(NOT_SELECT.isSame(NOT_SELECT), is(true));
+
+        assertThat(UNKNOWN.isSame(SELECT), is(false));
+        assertThat(UNKNOWN.isSame(UPDATE), is(false));
+        assertThat(UNKNOWN.isSame(INSERT), is(false));
+        assertThat(UNKNOWN.isSame(DELETE), is(false));
+        assertThat(UNKNOWN.isSame(UNKNOWN), is(true));
+        assertThat(UNKNOWN.isSame(NOT_SELECT), is(true));
     }
 }
