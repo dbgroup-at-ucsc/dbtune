dc779c7f067f62350ed369325dd0421ea9622d78
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-01-11 00:05:00 -0800

    Cosmetic change

--- src/edu/ucsc/dbtune/advisor/interactions/IndexInteraction.java
+++ src/edu/ucsc/dbtune/advisor/interactions/IndexInteraction.java
@@ -4,22 +4,29 @@ import edu.ucsc.dbtune.metadata.Index;
 
 public class IndexInteraction 
 {
-	private Index a;
-	private Index b;
-	
-	public IndexInteraction(Index _a, Index _b) 
-	{
-		a = _a;
-		b = _b;
-	}
-	
-	public Index getFirst()
-	{
-		return a;
-	}
-	
-	public Index getSecond()
-	{
-		return b;
-	}
+    private Index a;
+    private Index b;
+    private double interactionLevel;
+    
+    public IndexInteraction(Index a, Index b, double interactionLevel)
+    {
+        this.a = a;
+        this.b = b;
+        this.interactionLevel = interactionLevel;
+    }
+    
+    public Index getFirst()
+    {
+        return a;
+    }
+    
+    public Index getSecond()
+    {
+        return b;
+    }
+
+    public double getInteractionLevel()
+    {
+        return interactionLevel;
+    }
 }
