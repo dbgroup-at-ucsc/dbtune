9fd7cde6a45257d9da51bda4706469f4cdfbc8e3
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-01-11 00:15:18 -0800

    issue #146 Identifying what needs to be done

--- src/edu/ucsc/dbtune/advisor/interactions/IndexInteractionFinder.java
+++ src/edu/ucsc/dbtune/advisor/interactions/IndexInteractionFinder.java
@@ -7,6 +7,8 @@ import edu.ucsc.dbtune.metadata.Index;
 import edu.ucsc.dbtune.workload.Workload;
 
 /**
+ * XXX for issue #146 an implementation of this interface should port the InteractionSelector class 
+ * and its dependencies from Karl's repository
  * @author Quoc Trung Tran
  */
 public interface IndexInteractionFinder 
