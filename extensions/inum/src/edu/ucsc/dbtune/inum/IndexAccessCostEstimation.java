4da9ed12f9c553162dceb9cbec283b90320e71e8
Huascar A. Sanchez <huascar.sanchez@gmail.com>
2012-01-13 15:35:04 -0800

    cosmetic changes: fixed JavaDoc's description. Fixed imports warnings. Rename some of the methods found in the Combinations utility class ( now these methods have a more representative name)

--- extensions/inum/src/edu/ucsc/dbtune/inum/IndexAccessCostEstimation.java
+++ extensions/inum/src/edu/ucsc/dbtune/inum/IndexAccessCostEstimation.java
@@ -5,27 +5,22 @@ import java.util.Set;
 import edu.ucsc.dbtune.metadata.Index;
 
 /**
- * Whenever the query cost for some input configuration needs to be evaluated,
- * this type could be used to estimate the cost of accessing the indexes in
- * the input configuration.
+ * Whenever the query cost for some input configuration needs to be evaluated, this type could be
+ * used to estimate the cost of accessing the indexes in the input configuration.
  *
- * The sum of the index access costs for the entire input configuration is
- * added to that of the internal subplan to obtain the final query cost.
+ * The sum of the index access costs for the entire input configuration is added to that of the
+ * internal subplan to obtain the final query cost.
  *
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
-public interface IndexAccessCostEstimation
-{
+public interface IndexAccessCostEstimation {
   /**
-   * Estimate the cost of accessing the index in an input configuration.
-   * Computing only the individual index access costs is much faster than
-   * a full blown optimizer call.
-   * @param query
-   *    single query.
-   * @param indexes
-   *    indexes to be used in the index access cost calculation
-   * @return
-   *    the cost of accessing the given index.
+   * Estimate the cost of accessing the indexes in an input configuration. Computing only the
+   * individual index access costs is much faster than a full blown optimizer call.
+   *
+   * @param query single query.
+   * @param indexes indexes to be used in the index access cost calculation
+   * @return the cost of accessing the given index.
    */
   double estimateIndexAccessCost(String query, Set<Index> indexes);
 }
--- extensions/inum/src/edu/ucsc/dbtune/util/Combinations.java
+++ extensions/inum/src/edu/ucsc/dbtune/util/Combinations.java
@@ -1,77 +1,77 @@
 package edu.ucsc.dbtune.util;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-
+import edu.ucsc.dbtune.metadata.Index;
 import java.util.HashSet;
 import java.util.Set;
 
-import edu.ucsc.dbtune.metadata.Index;
-
 /**
- * ...
+ * Utility class for dealing with the generation of all possible combinations of a set.
  *
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
-public class Combinations
-{
-    private Combinations()
-    {
+public class Combinations {
+  private Combinations() {
+    throw new AssertionError("Utility class.");
+  }
+
+  /**
+   * recursively finds all possible combinations of interesting orders, given a length of the
+   * combination, that could be covered in a query. Indexes should implement the Comparable type
+   * before they could be enumerated with this method.
+   *
+   * @param elements the source to be combined in many ways.
+   * @param n length of each generated set. For example, let's assume we have a set containing 3
+   *      elements. Given the size of this set, this method will generate 8 subsets:
+   *      the empty set (n = 0), 3 singletons (n = 1), 3 subsets containing 2 elements (n = 2),
+   *      and 1 subset containing 3 elements (n = 3).
+   * @param <T> a type bound.
+   * @return a set of all combinations for the given elements.
+   */
+  public static <T> Set<Set<T>> setOfAllSubsetsOfLengthN(Iterable<T> elements, int n) {
+    final Set<Set<T>> result = Sets.newHashSet();
+    if (n == 0) {
+      result.add(Sets.<T>newHashSet());
+      return result;
     }
 
-    /**
-     * recursively finds all possible combinations of interesting orders, given a length of the combination,
-     * that could be covered in a query. Indexes should implement the Comparable type before they
-     * could be enumerated with this method.
-     * @param elements
-     *    the source to be combined in many ways.
-     * @param n
-     *    length
-     * @param <T>
-     *      a type bound.
-     * @return
-     *    a list of all combinations for the given elements.
-     */
-    public static <T> Set<Set<T>> findCrossProduct(Iterable<T> elements, int n)
-    {
-        final Set<Set<T>> result = Sets.newHashSet();
-        if (n == 0) {
-            result.add(Sets.<T>newHashSet());
-            return result;
-        }
+    final Set<Set<T>> crossProducts = setOfAllSubsetsOfLengthN(elements, n - 1);
 
-        final Set<Set<T>> crossProducts = findCrossProduct(elements, n - 1);
+    for (Set<T> crossproduct : crossProducts) {
+      for (T element : elements) {
+        if (crossproduct.contains(element)) { continue; }
+        final Set<T> list = Sets.newHashSet();
+        list.addAll(crossproduct);
+        if (list.contains(element)) { continue; }
+        list.add(element);
 
-        for (Set<T> crossproduct: crossProducts){
-            for (T element: elements){
-                if (crossproduct.contains(element)) { continue; }
-                final Set<T> list = Sets.newHashSet();
-                list.addAll(crossproduct);
-                if (list.contains(element))        { continue; }
-                list.add(element);
-
-                if (result.contains(list))         { continue; }
-                result.add(list);
-            }
-        }
-
-        return result;
+        if (result.contains(list)) { continue; }
+        result.add(list);
+      }
     }
 
-    public static Set<Set<Index>> findCrossProduct(Set<Index> source)
-    {
-        Set<Set<Index>>  result = Sets.newHashSet();
+    return result;
+  }
 
-        for (int i = 0; i <= source.size(); i++){
-            result.addAll(findCrossProduct(source, i));
-        }
+  /**
+   *
+   * @param elements the set to be combined in many ways.
+   * @see #setOfAllSubsetsOfLengthN(Iterable, int)
+   * @return a set of all combinations for the given elements.
+   */
+  public static Set<Set<Index>> setOfAllSubsets(Set<Index> elements) {
+    Set<Set<Index>> result = Sets.newHashSet();
 
-        final Set<Set<Index>> combinations = Sets.newHashSet();
-
-        for (Set<Index> each : result){
-            combinations.add(new HashSet<Index>(Lists.<Index>newArrayList(each)));
-        }
-
-        return combinations;
+    for (int i = 0; i <= elements.size(); i++) {
+      result.addAll(setOfAllSubsetsOfLengthN(elements, i));
     }
+
+    final Set<Set<Index>> combinations = Sets.newHashSet();
+
+    for (Set<Index> each : result) {
+      combinations.add(new HashSet<Index>(each));
+    }
+
+    return combinations;
+  }
 }
