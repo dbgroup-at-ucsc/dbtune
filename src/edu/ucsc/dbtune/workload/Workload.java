b2b893a2580e17fe49d305fc6fc5ff3a334a77f7
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-01-19 00:29:47 -0800

    fixes #161 Adding multi-line support for Workload
    Extends the Workload class so that it's capable of reading multi-line
    SQL scripts.
    
    Also adds a size() method to retrieve the number of statements contained
    in a workload.
    
    Updated the test accordingly.

--- src/edu/ucsc/dbtune/workload/Workload.java
+++ src/edu/ucsc/dbtune/workload/Workload.java
@@ -1,9 +1,8 @@
 package edu.ucsc.dbtune.workload;
 
-import java.io.Reader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.Iterable;
+import java.io.Reader;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -18,14 +17,35 @@ import edu.ucsc.dbtune.metadata.Schema;
  * @author Ivo Jimenez
  */
 public class Workload implements Iterable<SQLStatement>
-//public class WorkloadReader
 {
     private List<SQLStatement> sqls;
 
     /**
      * Creates a workload containing the set of SQL statements provided by the {@code 
-     * workloadStream} object. It's assumed that there's one statement per line and only one-line 
-     * comments (line beginning with string {@code "--"}).
+     * workloadStream} object. It's assumed that statements are delimited by <strong> ONLY ONE 
+     * </strong> {@code ';'} semi-colon character (see examples below) and only one-line comments ( 
+     * a line beginning with string {@code "--"}) are contained in the SQL script, i.e. multi-line 
+     * comments aren't supported.
+     * <p>
+     * The constructor can detect empty statements as long as there is one per line, i.e. the 
+     * following produces a Workload of {@link #size} 1:
+     * <pre>
+     * {@code .
+     * "SELECT * FROM bla ; \n" +
+     * "; \n" +
+     * "; \n" +
+     * }
+     * </pre>
+     * while the following:
+     * <pre>
+     * {@code .
+     * "SELECT * FROM bla ; ; ; ; \n" +
+     * "; \n" +
+     * "; \n" +
+     * }
+     * </pre>
+     * produces a workload of size 1, but with the first query being {@code SELECT * FROM bla ; ; 
+     * ;}, which will produce an error if gets executed by a JDBC-compliant driver.
      *
      * @param workloadStream
      *     stream that provides the set of SQL statements. One statement per line is assumed; 
@@ -35,28 +55,34 @@ public class Workload implements Iterable<SQLStatement>
      * @throws SQLException
      *     if a statement can't get a category assigned to it
      */
-    public Workload(Reader workloadStream) throws IOException,SQLException
+    public Workload(Reader workloadStream) throws IOException, SQLException
     {
         BufferedReader reader;
-        String         line;
-        String         lineLow;
+        StringBuilder sb;
+        String line;
 
-        sqls   = new ArrayList<SQLStatement>();
+        sqls = new ArrayList<SQLStatement>();
         reader = new BufferedReader(workloadStream);
+        sb = new StringBuilder();
 
         while ((line = reader.readLine()) != null) {
 
-            line    = line.trim();
-            lineLow = line.toLowerCase();
+            line = line.trim();
 
-            if (lineLow.startsWith("--")) {
+            if (line.startsWith("--"))
                 continue;
-            }
 
             if (line.endsWith(";")) {
-                sqls.add(new SQLStatement(line.substring(0, line.length()-1)));
+                sb.append(line.substring(0, line.length() - 1));
+
+                final String sql = sb.toString();
+
+                if (!sql.isEmpty())
+                    sqls.add(new SQLStatement(sb.toString()));
+
+                sb = new StringBuilder();
             } else {
-                sqls.add(new SQLStatement(line));
+                sb.append(line + "\n");
             }
         }
     }
@@ -65,7 +91,11 @@ public class Workload implements Iterable<SQLStatement>
      * Returns the statement at the given position (zero-indexing).
      *
      * @param i
-     *     index of the SQL statement retrieved
+     *      index of the SQL statement retrieved
+     * @return
+     *      the statement at the given position
+     * @throws IndexOutOfBoundsException
+     *      if the index is out of range ({@code index < 0 || index >= size()})
      */
     public SQLStatement get(int i)
     {
@@ -81,13 +111,32 @@ public class Workload implements Iterable<SQLStatement>
         return sqls.iterator();
     }
 
+    /**
+     * Returns the number of statements contained in the workload.
+     *
+     * @return
+     *      size of the workload
+     */
+    public int size()
+    {
+        return sqls.size();
+    }
+
+    /**
+     * Partitions the workload into subsets of statements (or subworkloads) where each corresponds 
+     * to a different schema.
+     *
+     * @return
+     *      a mapping containing a workload in each entry corresponding to a schema
+     */
+    public Map<Schema, Workload> getSchemaToWorkloadMapping()
+    {
+        // todo: in order to partition we need to parse and bind. Not doing it for now
+        throw new RuntimeException("not yet");
+    }
+
     // SQLStatement get(String sql) // return the statement corresponding to the given sql
     //                              // string. This would require string matching stuff, or
     //                              // even having to do some query processing (parse, rewrite
     //                              // views, query flattening, etc) in order to compare queries
-    
-    public Map<Schema, Workload> getSchemaToWorkloadMapping()
-    {
-        throw new RuntimeException("not yet");
-    }
 }
