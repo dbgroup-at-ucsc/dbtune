677c3c3a9b8bb58b75c6475a4d818c7f2c1856f3
Quoc Trung Tran <tqtrung@soe.ucsc.edu>
2012-01-20 15:47:35 -0800

    issue #158 Add a small workload to test
    The small work load file (smallworkload.sql) contains two statements is used to test the correctness of INUM and BIP.

--- resources/workloads/db2/tpch/smallworkload.sql
+++ resources/workloads/db2/tpch/smallworkload.sql
@@ -1,5 +1,19 @@
 -- query
 select
+	l_extendedprice, l_discount
+from
+	tpch.lineitem,
+	tpch.part
+where
+		p_partkey = l_partkey
+		and p_brand = 'Brand#13'
+		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
+		and l_quantity >= 6 and l_quantity <= 16
+		and p_size between 1 and 5
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON';
+-- query
+select
 				sum(ps_supplycost * ps_availqty) * 0.0001000000
 			from
 				tpch.partsupp,
