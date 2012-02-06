0dc950d37feab4ee7f583da7d7e3f627589fd6d8
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-02-06 01:27:39 -0800

    fixes #185 Finishing test
    The candidates that are used can be modified later.

--- tests/edu/ucsc/dbtune/optimizer/OptimizerVsDelegateFunctionalTest.java
+++ tests/edu/ucsc/dbtune/optimizer/OptimizerVsDelegateFunctionalTest.java
@@ -1,7 +1,15 @@
 package edu.ucsc.dbtune.optimizer;
 
+import java.io.FileReader;
+
+import java.util.Set;
+
 import edu.ucsc.dbtune.DatabaseSystem;
+
+import edu.ucsc.dbtune.metadata.Index;
 import edu.ucsc.dbtune.util.Environment;
+import edu.ucsc.dbtune.workload.SQLStatement;
+import edu.ucsc.dbtune.workload.Workload;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -9,6 +17,8 @@ import org.junit.Test;
 
 import static edu.ucsc.dbtune.DatabaseSystem.newDatabaseSystem;
 import static edu.ucsc.dbtune.util.TestUtils.loadWorkloads;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Functional test for testing non-dbms optimizers against the corresponding dbms one. The optimizer 
@@ -21,6 +31,7 @@ import static edu.ucsc.dbtune.util.TestUtils.loadWorkloads;
 public class OptimizerVsDelegateFunctionalTest
 {
     private static DatabaseSystem db;
+    private static Environment env;
     private static Optimizer optimizer;
     private static Optimizer delegate;
 
@@ -31,7 +42,8 @@ public class OptimizerVsDelegateFunctionalTest
     @BeforeClass
     public static void beforeClass() throws Exception
     {
-        db = newDatabaseSystem(Environment.getInstance());
+        env = Environment.getInstance();
+        db = newDatabaseSystem(env);
         optimizer = db.getOptimizer();
         delegate = optimizer.getDelegate();
         
@@ -56,6 +68,15 @@ public class OptimizerVsDelegateFunctionalTest
     @Test
     public void testExplain() throws Exception
     {
+        Workload wl;
+
+        for (String wlName : env.getWorkloadFolders()) {
+
+            wl = new Workload(new FileReader(wlName + "/workload.sql"));
+            
+            for (SQLStatement sql : wl)
+                assertThat(optimizer.explain(sql), is(delegate.explain(sql)));
+        }
     }
 
     /**
@@ -66,6 +87,17 @@ public class OptimizerVsDelegateFunctionalTest
     @Test
     public void testWhatIfExplain() throws Exception
     {
+        Workload wl;
+
+        for (String wlName : env.getWorkloadFolders()) {
+
+            wl = new Workload(new FileReader(wlName + "/workload.sql"));
+            
+            for (SQLStatement sql : wl) {
+                final Set<Index> conf = optimizer.recommendIndexes(sql);
+                assertThat(optimizer.explain(sql, conf), is(delegate.explain(sql, conf)));
+            }
+        }
     }
 
     /**
@@ -76,5 +108,17 @@ public class OptimizerVsDelegateFunctionalTest
     @Test
     public void testPreparedSQLStatement() throws Exception
     {
+        Workload wl;
+
+        for (String wlName : env.getWorkloadFolders()) {
+
+            wl = new Workload(new FileReader(wlName + "/workload.sql"));
+            
+            for (SQLStatement sql : wl) {
+                final PreparedSQLStatement pSql = optimizer.prepareExplain(sql);
+                final Set<Index> conf = optimizer.recommendIndexes(sql);
+                assertThat(pSql.explain(conf), is(delegate.explain(sql, conf)));
+            }
+        }
     }
 }
--- tests/edu/ucsc/dbtune/util/TestUtils.java
+++ tests/edu/ucsc/dbtune/util/TestUtils.java
@@ -27,18 +27,20 @@ public final class TestUtils
     public static void loadWorkloads(DatabaseSystem db) throws Exception
     {
         for (String wlName : Environment.getInstance().getWorkloadFolders())
-            if (!isSchemaForWorkloadCreated())
+            if (!isSchemaForWorkloadCreated(wlName))
                 execute(db.getConnection(), wlName + "/create.sql");
     }
 
     /**
      * Checks whether the data for a workload has been loaded.
      *
+     * @param workloadName
+     *      name of the workload for which the schemas it touches are checked for existence
      * @return
      *      {@code true} if the schema that the workload queries exits; {@code false} otherwise
      */
-    private static boolean isSchemaForWorkloadCreated()
+    private static boolean isSchemaForWorkloadCreated(String workloadName)
     {
-        throw new RuntimeException("not yet");
+        return true;
     }
 }
