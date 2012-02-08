12a09115fc7551e4ef70e7033c96da3db5db1e36
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-02-07 17:11:24 -0800

    issue #196 Adding utility methods for testing
    workloads() returns a list of workloads
    
    getBaseOptimizer() returns the base optimizer (eg. DB2Optimizer,
    PGOptimzier, etc..)

--- tests/edu/ucsc/dbtune/util/TestUtils.java
+++ tests/edu/ucsc/dbtune/util/TestUtils.java
@@ -1,6 +1,16 @@
 package edu.ucsc.dbtune.util;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.ucsc.dbtune.DatabaseSystem;
+import edu.ucsc.dbtune.optimizer.Optimizer;
+import edu.ucsc.dbtune.workload.Workload;
 
 import static edu.ucsc.dbtune.util.SQLScriptExecuter.execute;
 
@@ -16,6 +26,30 @@ public final class TestUtils
      */
     private TestUtils()
     {
+    }
+
+    /**
+     * Returns the base Optimizer, i.e. the DBMSOptimizer (eg. MySQLOptimizer, DB2Optimizer, etc...)
+     * <p>
+     * Keeps getting the delegate until there's no one. This is useful for when more than one 
+     * optimizer is layered on top of the base one (e.g. IBG on top of INUM on top of DB2) 
+     *
+     * @param optimizer
+     *      for which the base optimizer is retrieved
+     * @return
+     *      the base optimizer
+     */
+    public static Optimizer getBaseOptimizer(Optimizer optimizer)
+    {
+        if (optimizer.getDelegate() == null)
+            return optimizer;
+
+        Optimizer baseOptimizer = optimizer.getDelegate();
+
+        while (baseOptimizer.getDelegate() != null)
+            baseOptimizer = optimizer.getDelegate();
+
+        return baseOptimizer;
     }
 
     /**
@@ -42,5 +76,29 @@ public final class TestUtils
     private static boolean isSchemaForWorkloadCreated(String workloadName)
     {
         return true;
+    }
+
+    /**
+     * Returns a list of {@link Workload} objects containing all the workloads in the given fully 
+     * qualified list of workload names.
+     *
+     * @param fullyQualifiedWorkloadNames
+     *      for each, an instance of {@link Workload} is created
+     * @return
+     *      list of workloads
+     * @throws IOException
+     *      if one of the files doesn't exist
+     * @throws SQLException
+     *      if {@link Workload} constructor throws an exception
+     */
+    public static List<Workload> workloads(List<String> fullyQualifiedWorkloadNames)
+        throws IOException, SQLException
+    {
+        List<Workload> workloads = new ArrayList<Workload>();
+
+        for (String wlName : fullyQualifiedWorkloadNames)
+            workloads.add(new Workload(new FileReader(wlName + "/workload.sql")));
+
+        return workloads;
     }
 }
