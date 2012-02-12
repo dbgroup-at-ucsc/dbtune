bf618fdf2a0d121c65b2bdc0a230fb6a919cbaef
Ivo Jimenez <ivo@cs.ucsc.edu>
2012-02-12 02:23:49 -0800

    fixes #196 testing for InumPlan
    Adds a test to check that the buildQueryForUnseenIndex works as expected

--- extensions/inum/tests/edu/ucsc/dbtune/optimizer/plan/InumPlanTest.java
+++ extensions/inum/tests/edu/ucsc/dbtune/optimizer/plan/InumPlanTest.java
@@ -1,6 +1,20 @@
 package edu.ucsc.dbtune.optimizer.plan;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsc.dbtune.metadata.Catalog;
+import edu.ucsc.dbtune.metadata.Column;
+import edu.ucsc.dbtune.metadata.Index;
+import edu.ucsc.dbtune.metadata.Table;
+import edu.ucsc.dbtune.workload.SQLStatement;
+
 import org.junit.Test;
+
+import static edu.ucsc.dbtune.DBTuneInstances.configureCatalog;
+
+import static edu.ucsc.dbtune.metadata.Index.ASC;
+import static edu.ucsc.dbtune.metadata.Index.DESC;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -52,5 +66,75 @@ public class InumPlanTest
         sqlPlan = mock(SQLStatementPlan.class);
         when(sqlPlan.getParent(tblScan)).thenReturn(join);
         assertThat(InumPlan.extractCostOfLeaf(sqlPlan, tblScan), is(tblScan.getAccumulatedCost()));
+    }
+
+    /**
+     * @throws Exception
+     *      if fails
+     */
+    @Test
+    public void testBuildingOfQueryForUnseenIndex() throws Exception
+    {
+        Catalog catalog = configureCatalog();
+
+        TableAccessSlot slot = mock(TableAccessSlot.class);
+
+        Table table = catalog.<Table>findByName("schema_0.table_0");
+        Index index = catalog.<Index>findByName("schema_0.table_0_index_2");
+
+        when(slot.getTable()).thenReturn(table);
+        when(slot.getIndex()).thenReturn(index);
+
+        InterestingOrder io = new InterestingOrder(table.columns().get(0), ASC);
+
+        when(slot.getColumnsFetched()).thenReturn(io);
+
+        SQLStatement sql = InumPlan.buildQueryForUnseenIndex(slot);
+
+        assertThat(sql.getSQL().contains("SELECT"), is(true));
+        assertThat(sql.getSQL().contains("FROM " + table.getFullyQualifiedName()), is(true));
+        assertThat(sql.getSQL().contains("WHERE"), is(false));
+        assertThat(sql.getSQL().contains("ORDER"), is(true));
+
+        for (Column c : index.columns())
+            assertThat(
+                sql.getSQL().contains(c.getName() + (index.isAscending(c) ? " ASC" : " DESC")),
+                is(true));
+
+        io.add(table.columns().get(1), DESC);
+        io.add(table.columns().get(2), ASC);
+        io.add(table.columns().get(3), DESC);
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        predicates.add(new Predicate(null, "A > 10"));
+        predicates.add(new Predicate(null, "B > 10"));
+        predicates.add(new Predicate(null, "C > 10"));
+
+        slot = mock(TableAccessSlot.class);
+
+        when(slot.getTable()).thenReturn(table);
+        when(slot.getIndex()).thenReturn(io);
+        when(slot.getColumnsFetched()).thenReturn(io);
+        when(slot.getPredicates()).thenReturn(predicates);
+
+        sql = InumPlan.buildQueryForUnseenIndex(slot);
+
+        assertThat(sql.getSQL().contains("SELECT"), is(true));
+        assertThat(sql.getSQL().contains("FROM " + table.getFullyQualifiedName()), is(true));
+        assertThat(sql.getSQL().contains("WHERE"), is(true));
+        assertThat(sql.getSQL().contains("ORDER"), is(true));
+
+        for (Column c : io.columns()) {
+            assertThat(
+                sql.getSQL().contains(c.getName()), is(true));
+            assertThat(
+                sql.getSQL().contains(c.getName() + (io.isAscending(c) ? " ASC" : " DESC")),
+                is(true));
+        }
+
+        assertThat(sql.getSQL().contains("A > 10"), is(true));
+        assertThat(sql.getSQL().contains("B > 10"), is(true));
+        assertThat(sql.getSQL().contains("C > 10"), is(true));
     }
 }
