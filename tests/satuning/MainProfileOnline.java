a90401f8e29eed7aec9679727f5896401af2e293
Ivo Jimenez <ivo.jimenez@gmail.com>
2012-03-16 15:45:49 -0700

    Removing unused files

--- tests/satuning/MainProfileOnline.java
+++
@@ -1,6 +0,0 @@
-package satuning;
-
-public class MainProfileOnline {
-	
-
-}
--- tests/satuning/admin/WorkloadRunner.java
+++
@@ -1,9 +0,0 @@
-package satuning.admin;
-
-import satuning.engine.ProfiledQuery;
-import satuning.engine.selection.WorkFunctionAlgorithm;
-import satuning.util.BitSet;
-
-public interface WorkloadRunner {
-	public BitSet[] getRecs(ProfiledQuery[] qinfos, WorkFunctionAlgorithm wfa);
-}
