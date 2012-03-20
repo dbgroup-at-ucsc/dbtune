2332e699aaa586e45920220e16ac31d85ff2db69
Quoc Trung Tran <tqtrung@soe.ucsc.edu>
2012-03-20 10:08:29 -0700

    issue #248 The code to repeat the underestimation of INUM.
    - Run DivBIPFunctionalTest to repeat the test case (the workload is now in tpcds-debug/workload.sql).

--- extensions/bip/tests/edu/ucsc/dbtune/bip/DivBIPFunctionalTest.java
+++ extensions/bip/tests/edu/ucsc/dbtune/bip/DivBIPFunctionalTest.java
@@ -23,8 +23,11 @@ import edu.ucsc.dbtune.bip.div.DivConstraint;
 import edu.ucsc.dbtune.bip.div.DivergentOnOptimizer;
 import edu.ucsc.dbtune.bip.util.LogListener;
 import edu.ucsc.dbtune.metadata.Index;
+import edu.ucsc.dbtune.optimizer.ExplainedSQLStatement;
 import edu.ucsc.dbtune.optimizer.InumOptimizer;
+import edu.ucsc.dbtune.optimizer.InumPreparedSQLStatement;
 import edu.ucsc.dbtune.optimizer.Optimizer;
+import edu.ucsc.dbtune.optimizer.plan.InumPlan;
 import edu.ucsc.dbtune.util.Environment;
 import edu.ucsc.dbtune.workload.SQLStatement;
 import edu.ucsc.dbtune.workload.Workload;
@@ -59,10 +62,10 @@ public class DivBIPFunctionalTest extends BIPTestConfiguration
         List<DivConstraint> constraints = new ArrayList<DivConstraint>();
         constraints.add(iReplica);
         //constraints.add(iFailure);
-        ConstraintDivBIP div = new ConstraintDivBIP(constraints);
+        //ConstraintDivBIP div = new ConstraintDivBIP(constraints);
+        DivBIP div = new DivBIP();
         
-        
-        Workload workload = workload(en.getWorkloadsFoldername() + "/tpcds-small");
+        Workload workload = workload(en.getWorkloadsFoldername() + "/tpcds-debug");
         CandidateGenerator candGen = 
             new OptimizerCandidateGenerator(getBaseOptimizer(db.getOptimizer()));
         Set<Index> candidates = candGen.generate(workload);
@@ -72,7 +75,8 @@ public class DivBIPFunctionalTest extends BIPTestConfiguration
         System.out.println("L59, number of candidate: " + candidates.size());
         
         // At most three indexes are deployed at one replica
-        B = 3;
+        B = 10;
+        
         
         Optimizer io = db.getOptimizer();
 
@@ -89,12 +93,13 @@ public class DivBIPFunctionalTest extends BIPTestConfiguration
         div.setLogListenter(logger);
         
         IndexTuningOutput output = div.solve();
-        div.exportCplexToFile(en.getWorkloadsFoldername() + "/tpcds-small/test.lp");
+        div.exportCplexToFile(en.getWorkloadsFoldername() + "/tpcds-debug/test.lp");
         System.out.println(logger.toString());
         if (output != null) {
             System.out.println("In test, result: " 
                               + " obj value: " + div.getObjValue());
-            div.costFromCplex();
+            //div.costFromCplex();
+            
             /*
             // run on actual optimize
             Set<SQLStatement> sqls = new HashSet<SQLStatement>();
@@ -105,21 +110,48 @@ public class DivBIPFunctionalTest extends BIPTestConfiguration
             
             System.out.println("L90, cost on DB2: " + doo.getTotalCost()
                                 + " COST RATIO: " + (double) doo.getTotalCost() / div.getObjValue());
-            
+            */
             // test
             DivConfiguration divconf = (DivConfiguration) output;
             Set<Index> conf = divconf.indexesAtReplica(0);
-            System.out.println(" query INUM DB2 INUM/DB2 ");
-            double costInum, costDB2;
+            System.out.println(" Configuration: ");
+            for (Index index : conf) 
+                System.out.print(" " + index + " " + index.getId());
+                
+            System.out.println("\n query INUM DB2 INUM/DB2 ");
+            double costInum, costDB2, ratio;
+            
             for (int i = 0; i < workload.size(); i++){
-                costInum = io.prepareExplain(workload.get(i)).explain(conf).getTotalCost();
-                costDB2  = io.getDelegate().explain(workload.get(i), conf).getTotalCost();
-                System.out.println(i 
-                                   + " " + costInum
-                                   + " " + costDB2
-                                   + " " + (costInum / costDB2));
+                
+                System.out.println(" ---- query " + i + "\n");
+                InumPreparedSQLStatement inumStmt = (InumPreparedSQLStatement)
+                                                io.prepareExplain(workload.get(i));
+                ExplainedSQLStatement inumExplain = inumStmt.explain(conf);
+                ExplainedSQLStatement db2Explain  = io.getDelegate().explain(workload.get(i), conf);
+                                                  
+                costInum = inumExplain.getTotalCost();
+                costDB2  = db2Explain.getTotalCost();
+                
+                ratio = costInum / costDB2;
+                
+                if (ratio >= 1.5 || ratio <= 0.5 && i == 1) {                    
+                    System.out.println(i 
+                                       + " " + costInum
+                                       + " " + costDB2
+                                       + " " + (costInum / costDB2));
+                    System.out.println(" template \n");
+                    for (InumPlan plan : inumStmt.getTemplatePlans()) {
+                        System.out.println(plan + "\n " + 
+                                           " internal plan cost: " + plan.getInternalCost());
+                    }
+                    System.out.println(" instantiated INUM: \n " + inumExplain.getPlan()
+                                        + " \n COST: " + costInum);
+                    
+                    System.out.println(" instantiated DB2: \n" + db2Explain.getPlan()
+                                        + " \n COST: " + costDB2);
+              }
             }
-            */
+            
         } else {
             System.out.println(" NO SOLUTION ");
         }
