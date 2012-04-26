a45699a16b00db5880866f2734dda1040be55528
Quoc Trung Tran <tqtrung@soe.ucsc.edu>
2012-04-25 17:26:47 -0700

    fixes #271 Add SSB workload
    - The workload is manually copied from the paper.
    
    - There are 13 queries, 3 of which cannot run with INUM currently (the duplicate operator issue).
    
    - This workload seems not to be an interesting example for Divergent, since most of the indexes recommended by DB2 have very small sizes, except indexes defined on the biggest table "lineorder".
    
    - For a mixed workload (queries & inserts, deletes), the divergent algorithms will recommend indexes that do not belong to lineorder.

---
+++ resources/workloads/db2/ssb-tpch/workload.sql
@@ -0,0 +1,315 @@
+--query 1.1
+select 
+	sum(lo_extendedprice*lo_discount) as revenue
+from 
+	ssb.lineorder, 
+	ssb.date
+where 
+	lo_orderdate = d_datekey
+	and d_year = 1993
+	and lo_discount between 1 and 3
+	and lo_quantity < 25 ;
+
+-- query 1.2
+select 
+	sum(lo_extendedprice*lo_discount) as revenue
+from 
+	ssb.lineorder, 
+	ssb.date
+where 
+	lo_orderdate = d_datekey
+	and d_yearmonthnum = 199401
+	and lo_discount between 4 and 6
+	and lo_quantity between 26 and 35;
+
+-- query 1.3
+select 
+	sum(lo_extendedprice*lo_discount) as revenue
+from 
+	ssb.lineorder, 
+	ssb.date
+where 
+	lo_orderdate = d_datekey
+	and d_weeknuminyear = 6
+	and d_year = 1994
+	and lo_discount between 5 and 7
+	and lo_quantity between 26 and 35 ;
+
+-- query 2.1
+select 
+	sum(lo_revenue), 
+	d_year, 
+	p_brand
+from 
+	ssb.lineorder, 
+	ssb.date, 
+	ssb.part, 
+	ssb.supplier
+where 
+	lo_orderdate = d_datekey
+	and lo_partkey = p_partkey
+	and lo_suppkey = s_suppkey
+	and p_category = 'MFGR#12'
+	and s_region = 'AMERICA'
+group by 
+	d_year, 
+	p_brand
+order by 
+	d_year, 
+	p_brand;
+
+
+-- query 2.2.
+select 
+	sum(lo_revenue), 
+	d_year, 
+	p_brand
+from 
+	ssb.lineorder, 
+	ssb.date, 
+	ssb.part, 
+	ssb.supplier
+where 
+	lo_orderdate = d_datekey
+	and lo_partkey = p_partkey
+	and lo_suppkey = s_suppkey
+	and p_brand between 'MFGR#2221' and 'MFGR#2228'
+	and s_region = 'ASIA'
+group by 
+	d_year, 
+	p_brand
+order by 
+	d_year, 
+	p_brand;
+
+-- query 2.3
+select 
+	sum(lo_revenue), 
+	d_year, 
+	p_brand
+from 
+	ssb.lineorder, 
+	ssb.date, 
+	ssb.part, 
+	ssb.supplier
+where 
+	lo_orderdate = d_datekey
+	and lo_partkey = p_partkey
+	and lo_suppkey = s_suppkey
+	and p_brand = 'MFGR#2221'
+	and s_region = 'EUROPE'
+group by 
+	d_year, 
+	p_brand
+order by 
+	d_year, 
+	p_brand;
+
+-- query 3.1
+select 
+	c_nation, 
+	s_nation, 
+	d_year, 
+	sum(lo_revenue) as revenue 
+from 
+	ssb.customer, 
+	ssb.lineorder, 
+	ssb.supplier, 
+	ssb.date
+where 
+	lo_custkey = c_custkey
+	and lo_suppkey = s_suppkey
+	and lo_orderdate = d_datekey
+	and c_region = 'ASIA' and s_region = 'ASIA'
+	and d_year >= 1992 and d_year <= 1997
+group by 
+	c_nation, 
+	s_nation, 
+	d_year
+order by 
+	d_year asc, 
+	revenue desc;
+
+-- query 3.2.
+-- ERROR: duplicate operator found
+--select 
+--	c_city, 
+--	s_city, 
+--	d_year, 
+--	sum(lo_revenue) as revenue 
+--from 
+--	ssb.customer, 
+--	ssb.lineorder, ssb.supplier, 
+--	ssb.date
+--where 
+--	lo_custkey = c_custkey
+--	and lo_suppkey = s_suppkey
+--	and lo_orderdate = d_datekey
+--	and c_nation = 'UNITED STATES'
+--	and s_nation = 'UNITED STATES'
+--	and d_year >= 1992 and d_year <= 1997
+--group by 
+--	c_city, 
+--	s_city, 
+--	d_year
+--order by 
+--	d_year asc, 
+--	revenue desc;
+
+-- query 3.3.
+select 
+	c_city, 
+	s_city, 
+	d_year, 
+	sum(lo_revenue) as revenue 
+from 
+	ssb.customer, 
+	ssb.lineorder, 
+	ssb.supplier, 
+	ssb.date
+where 
+	lo_custkey = c_custkey
+	and lo_suppkey = s_suppkey
+	and lo_orderdate = d_datekey
+	and (c_city='UNITED KI1' or c_city='UNITED KI5')
+	and d_year >= 1992 and d_year <= 1997
+group by 
+	c_city, 
+	s_city, 
+	d_year
+order by 
+	d_year asc, 
+	revenue desc;
+
+-- query 3.4.
+-- Duplicate operator
+--select 
+--	c_city, 
+--	s_city, 
+--	d_year, 
+--	sum(lo_revenue) as revenue 
+--from 
+--	ssb.customer, 
+--	ssb.lineorder, 
+--	ssb.supplier, 
+--	ssb.date
+--where 
+--	lo_custkey = c_custkey
+--	and lo_suppkey = s_suppkey
+--	and lo_orderdate = d_datekey
+--	and (c_city='UNITED KI1' or c_city='UNITED KI5')
+--	and (s_city='UNITED KI1' or s_city='UNITED KI5')
+--	and d_yearmonth = 'Dec1997'
+--group by 
+--	c_city, 
+--	s_city, 
+--	d_year
+--order by 
+--	d_year asc, 
+--	revenue desc;
+
+-- query 4.1
+select 
+	d_year, 
+	c_nation, 
+	sum(lo_revenue - lo_supplycost) as profit 
+from 
+	ssb.date, 
+	ssb.customer, 
+	ssb.supplier,
+	ssb.part, 
+	ssb.lineorder
+where 
+	lo_custkey = c_custkey
+	and lo_suppkey = s_suppkey
+	and lo_partkey = p_partkey
+	and lo_orderdate = d_datekey
+	and c_region = 'AMERICA'
+	and s_region = 'AMERICA'
+	and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+group by 
+	d_year, 
+	c_nation
+order by 
+	d_year, 
+	c_nation;
+
+--- query 4.2
+select 
+	d_year, 
+	s_nation, 
+	p_category,
+	sum(lo_revenue - lo_supplycost) as profit
+from 
+	ssb.date, 
+	ssb.customer, 
+	ssb.supplier, 
+	ssb.part, 
+	ssb.lineorder
+where 
+	lo_custkey = c_custkey
+	and lo_suppkey = s_suppkey
+	and lo_partkey = p_partkey
+	and lo_orderdate = d_datekey
+	and c_region = 'AMERICA'
+	and s_region = 'AMERICA'
+	and (d_year = 1997 or d_year = 1998)
+	and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+group by 
+	d_year, 
+	s_nation, 
+	p_category
+order by 
+	d_year, 
+	s_nation, 
+	p_category; 
+
+--- query 4.3
+
+--select 
+--	d_year, 
+--	s_city, 
+--	p_brand, 
+--	sum(lo_revenue - lo_supplycost) as profit
+--from 
+--	ssb.date, 
+--	ssb.customer, 
+--	ssb.supplier, 
+--	ssb.part, 
+--	ssb.lineorder
+--where 
+--	lo_custkey = c_custkey
+--	and lo_suppkey = s_suppkey
+--	and lo_partkey = p_partkey
+--	and lo_orderdate = d_datekey
+--	and c_region = 'AMERICA'
+--	and s_nation = 'UNITED STATES'
+--	and (d_year = 1997 or d_year = 1998)
+--	and p_category = 'MFGR#14'
+--group by 
+--	d_year, 
+--	s_city, 
+--	p_brand
+--order by 
+--	d_year, 
+--	s_city, 
+--	p_brand ; 
+
+-- DELETE statement
+DELETE FROM ssb.lineorder WHERE lo_orderkey = 36;
+-- INSERT statement
+INSERT INTO ssb.lineorder VALUES(9,1,7381,96555,56,19980706,'5-LOW',0,30,4654650,5849222,9,4235731, 93093, 0, 19980918, `FOB'S) 
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
