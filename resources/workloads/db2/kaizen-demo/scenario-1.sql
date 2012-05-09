d398dd59a0e3833c52102675fc8ff6bab6c2420a
Ivo Jimenez <ivo.jimenez@gmail.com>
2012-05-08 20:44:27 -0700

    Minor change on scenario 1 workload

--- resources/workloads/db2/kaizen-demo/scenario-1.sql
+++ resources/workloads/db2/kaizen-demo/scenario-1.sql
@@ -2,9 +2,9 @@ SELECT a FROM one_table.tbl WHERE a = 2;
 SELECT a FROM one_table.tbl WHERE a = 2;
 
 UPDATE one_table.tbl set a = a+1 WHERE a BETWEEN        0 AND  5000000;
-UPDATE one_table.tbl set a = a+1 WHERE a BETWEEN  5000000 AND  9000000;
-UPDATE one_table.tbl set a = a+1 WHERE a BETWEEN  9000000 AND 11000000;
-UPDATE one_table.tbl set a = a+1 WHERE a BETWEEN 11000000 AND 15000000;
+UPDATE one_table.tbl set a = a+2 WHERE a BETWEEN  5000000 AND  9000000;
+UPDATE one_table.tbl set a = a+3 WHERE a BETWEEN  9000000 AND 11000000;
+UPDATE one_table.tbl set a = a+4 WHERE a BETWEEN 11000000 AND 15000000;
 
 SELECT a FROM one_table.tbl WHERE a = 2;
 SELECT a FROM one_table.tbl WHERE b = 2;
