86030f368e8caf5679e3621dc6a4558dcf5fd374
Ivo Jimenez <ivo.jimenez@gmail.com>
2012-05-21 18:10:33 -0700

    updating scenario 1

--- resources/workloads/db2/kaizen-demo/scenario-1.sql
+++ resources/workloads/db2/kaizen-demo/scenario-1.sql
@@ -1,31 +1,25 @@
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
 
-UPDATE one_table.tbl set col1 = col1+1 WHERE col1 BETWEEN        0 AND   500000;
-UPDATE one_table.tbl set col1 = col1+2 WHERE col1 BETWEEN   500000 AND   900000;
-UPDATE one_table.tbl set col1 = col1+3 WHERE col1 BETWEEN  9000000 AND 11000000;
-
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-SELECT col1 FROM one_table.tbl WHERE col1 = 2;
-SELECT col1 FROM one_table.tbl WHERE col1 = 2;
-
-UPDATE one_table.tbl set col1 = col1+1 WHERE col1 BETWEEN        0 AND   500000;
-UPDATE one_table.tbl set col1 = col1+2 WHERE col1 BETWEEN  500000 AND  900000;
-UPDATE one_table.tbl set col1 = col1+3 WHERE col1 BETWEEN  9000000 AND 11000000;
+UPDATE one_table.tbl set col1 = col1+1 WHERE col1 = 2;
 
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-
-UPDATE one_table.tbl set col1 = col1+1 WHERE col1 BETWEEN        0 AND   500000;
-UPDATE one_table.tbl set col1 = col1+2 WHERE col1 BETWEEN  500000 AND  900000;
-UPDATE one_table.tbl set col1 = col1+3 WHERE col1 BETWEEN  9000000 AND 11000000;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
 
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
 SELECT col1 FROM one_table.tbl WHERE col1 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
-SELECT col1 FROM one_table.tbl WHERE col2 = 2;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+
+SELECT col1 FROM one_table.tbl WHERE col1 = 2;
+SELECT col1 FROM one_table.tbl WHERE col1 = 2;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
+SELECT col2 FROM one_table.tbl WHERE col1 between 0 AND 20000000 AND col2 = 0;
