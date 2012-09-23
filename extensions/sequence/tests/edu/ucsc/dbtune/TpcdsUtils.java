255df8bb05c8d35d9f56260b82ecf0054a4b0a1b
Rui Wang <rwang10@ucsc.edu>
2012-09-23 10:46:42 -0700

    issue #314 update tpcds workload
    I compared the new tpcds workload Jeff sent me. There are several
    minor changes, like change stddev to stddev_samp, add alias to
    subquery result.
    
    There are 99 query templates. For 4 of them, two different queries
    are generated. I thought it's duplication so I comment them out
    but they are not. The new workload also has 103 queries.

---
+++ extensions/sequence/tests/edu/ucsc/dbtune/TpcdsUtils.java
@@ -0,0 +1,63 @@
+package edu.ucsc.dbtune;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.regex.Pattern;
+
+import edu.ucsc.dbtune.util.Rt;
+import edu.ucsc.dbtune.workload.Workload;
+
+/**
+ * This file is used to compare the difference between two tpcds workload, can
+ * be removed.
+ * 
+ * @author wangrui
+ * 
+ */
+public class TpcdsUtils {
+    public static void main(String[] args) throws Exception {
+        String tpcdsNew = Rt.readFile(new File(
+                "resources/workloads/db2/tpcds/queriesWithTemplateNumber.sql"));
+        String tpcdsOld = Rt.readFile(new File(
+                "resources/workloads/db2/tpcds/db2.sql"));
+        Workload workload = new Workload("", new StringReader(tpcdsOld));
+        int n = 0;
+        for (String line2 : tpcdsNew.split("\n")) {
+            line2 = line2.substring(line2.indexOf(' ') + 1).trim();
+            String[] ss = line2.split(";");
+            for (String line : ss) {
+                line = line.trim();
+                String sql = workload.get(n++).getSQL().trim();
+                sql = sql.replaceAll(Pattern.quote("tpcds."), "");
+                line = line.replaceAll("\\s+", " ");
+                sql = sql.replaceAll("\\s+", " ");
+                line = line.replaceAll(" ,", ",");
+                sql = sql.replaceAll(" ,", ",");
+                line = line.replaceAll(", ", ",");
+                sql = sql.replaceAll(", ", ",");
+                line = line.replaceAll("\\( ", "(");
+                sql = sql.replaceAll("\\( ", "(");
+                line = line.replaceAll(" \\+ ", "+");
+                sql = sql.replaceAll(" \\+ ", "+");
+                line = line.replaceAll(" \\= ", "=");
+                sql = sql.replaceAll(" \\= ", "=");
+                if (!line.equalsIgnoreCase(sql)) {
+                    line = line.toLowerCase();
+                    sql = sql.toLowerCase();
+                    for (int i = 0; i < line.length() && i < sql.length(); i++) {
+                        char c1 = line.charAt(i);
+                        char c2 = sql.charAt(i);
+                        if (c1 != c2) {
+                            line = line.substring(Math.max(0, i - 10));
+                            sql = sql.substring(Math.max(0, i - 10));
+                            break;
+                        }
+                    }
+                    Rt.np(line);
+                    Rt.np(sql);
+                }
+            }
+        }
+    }
+}
